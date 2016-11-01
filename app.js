53a1d28560adcb7ccda2da2caf4e9ce91affb800
Cody Chan <int64ago@gmail.com>
2016-11-02 00:27:41 +0800

    Use express as server engine

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello NEK!');
+});
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000!');
+});
