4e09927038b6e38549ab88fca8e4dffbd177a53b
Cody Chan <int64ago@gmail.com>
2016-11-02 21:15:46 +0800

    Utablify

--- webpack.config.js
+++ webpack.config.js
@@ -3,31 +3,31 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: [
-		'./public/index.js',
-	],
-	devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
-	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: 'bundle-[hash].js',
-	},
-	resolve: {
-		alias: {
-			'rgui_css': path.join(__dirname, 'node_modules/nek-ui/dist/css'),
-		},
-	},
-	module: {
-		loaders: [
-			{ test: /\.html$/, loader: 'raw' },
-			{ test: /\.css$/, loader: 'style!css' },
-			{ test: /\.scss$/, loader: 'style!css!sass' },
-			{
-				test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
-				loader: 'url?limit=100000&name=[name].[ext]',
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin(),
-	],
+  entry: [
+    './public/index.js',
+  ],
+  devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle-[hash].js',
+  },
+  resolve: {
+    alias: {
+      'rgui_css': path.join(__dirname, 'node_modules/nek-ui/dist/css'),
+    },
+  },
+  module: {
+    loaders: [
+      { test: /\.html$/, loader: 'raw' },
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.scss$/, loader: 'style!css!sass' },
+      {
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
+        loader: 'url?limit=100000&name=[name].[ext]',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+  ],
 };
