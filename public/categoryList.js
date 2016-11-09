c465ca9bd48bd73cea460e003b8313ce599f0282
Cody Chan <int64ago@gmail.com>
2016-11-09 20:47:50 +0800

    Write categoryList in single file

---
+++ public/categoryList.js
@@ -0,0 +1,29 @@
+const categoryList = [{
+  title: "简单组件",
+  moduleList: [{
+      desc: "按钮",
+      name: 'nek.button',
+      cols: 2,
+      icon: "http://placeholder.cn/80",
+    }, {
+      desc: "标签",
+      name: 'nek.label',
+      cols: 1,
+      icon: "http://placeholder.cn/80",
+    }]
+}, {
+  title: "基础组件",
+  moduleList: [{
+      desc: "输入框",
+      name: 'nek.input',
+      cols: 2,
+      icon: "http://placeholder.cn/80",
+    }, {
+      desc: "选择框",
+      name: 'nek.select',
+      cols: 2,
+      icon: "http://placeholder.cn/80",
+    }]
+}];
+
+module.exports = categoryList;
--- public/modules/module.list/index.js
+++ public/modules/module.list/index.js
@@ -1,5 +1,7 @@
 const RGUI = require('nek-ui');
 const tpl = require('!raw!./index.html');
+const categoryList = require('../../categoryList');
+
 import {  Draggable, Droppable, Movable } from 'rgui-ui-drag'
 
 
@@ -9,51 +11,7 @@ const ModuleList = RGUI.Component.extend({
     config: function() {
         this.defaults({
             tdWidth: 40,
-            list: [
-                {
-                    category: {
-                        name: '基础组件',
-                        modules: [{
-                            name: 'form.input',
-                            moduleId: 1,
-                            width: 5,
-                            height: 1
-                        }, {
-                            name: 'form.number',
-                            moduleId: 2,
-                            width: 4,
-                            height: 2
-                        },
-                        {
-                            name: 'form.number',
-                            moduleId: 3,
-                            width: 5,
-                            height: 3
-                        }]
-                    }
-                },
-                {
-                    category: {
-                        name: '业务组件',
-                        modules: [{
-                            name: 'form.input',
-                            moduleId: 4,
-                            width: 24,
-                            height: 5
-                        }, {
-                            name: 'form.input',
-                            moduleId: 5,
-                            width: 10,
-                            height: 3
-                        }, {
-                            name: 'form.number',
-                            moduleId: 6,
-                            width: 12,
-                            height: 6
-                        }]
-                    }
-                }
-            ]
+            categoryList: categoryList,
         });
         this.supr();
     }
