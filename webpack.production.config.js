fab66c6a0e613bcd0c01c113fadb268ec081be91
Cody Chan <int64ago@gmail.com>
2016-11-10 15:35:29 +0800

    Fix sass error in ExtractTextPlugin

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -34,7 +34,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('style', 'css', 'sass'),
+        loader: ExtractTextPlugin.extract('style', 'css!sass'),
       },
       {
         test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
