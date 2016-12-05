775210a02126c6afb2b43e5d417351eddf48aeb8
Cody Chan <int64ago@gmail.com>
2016-12-05 20:32:56 +0800

    Enable add / edit project

--- public/modules/home/index.js
+++ public/modules/home/index.js
@@ -1,6 +1,7 @@
 /* global fetch: false */
 import { Component } from 'nek-ui';
 import template from '!raw!./index.html';
+import EditModal from './edit.modal';
 
 const Home = Component.extend({
   name: 'home',
@@ -16,6 +17,32 @@ const Home = Component.extend({
   init() {
     this._getProjects();
     this.supr();
+  },
+
+  edit(event, project = {}) {
+    event.stopPropagation();
+    const modal = new EditModal({ data: { project } });
+    modal.$on('upsertProject', (params) => {
+      this._upsertProject(params);
+    });
+  },
+
+  _upsertProject(params) {
+    fetch('/api/project/upsert', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify(Object.assign(params, {
+        project: params._id,
+      })),
+    })
+    .then(res => res.json())
+    .then((json) => {
+      this._getProjects();
+    }).catch((err) => {
+      console.error(err.message);
+    });
   },
 
   _getProjects() {
