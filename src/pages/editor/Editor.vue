0dd476216c6f818812d6aa1cd7d79b442e39d148
王松 <wangsong@corp.netease.com>
2017-09-19 19:03:32 +0800

    Demo

--- src/pages/editor/Editor.vue
+++ src/pages/editor/Editor.vue
@@ -11,23 +11,52 @@
               <li>
                 <a href="javascript:;" draggable="true" @dragstart="dragStart($event, 'kl-button')">kl-button</a>
               </li>
+              <li>
+                <a href="javascript:;" draggable="true" @dragstart="dragStart($event, 'kl-row')">kl-row</a>
+              </li>
+              <li>
+                <a href="javascript:;" draggable="true" @dragstart="dragStart($event, 'kl-col-1')">kl-col-1</a>
+              </li>
+              <li>
+                <a href="javascript:;" draggable="true" @dragstart="dragStart($event, 'kl-col-2')">kl-col-2</a>
+              </li>
+              <li>
+                <a href="javascript:;" draggable="true" @dragstart="dragStart($event, 'kl-col-3')">kl-col-3</a>
+              </li>
+              <li>
+                <a href="javascript:;" draggable="true" @dragstart="dragStart($event, 'kl-col-4')">kl-col-4</a>
+              </li>
+              <li>
+                <a href="javascript:;" draggable="true" @dragstart="dragStart($event, 'kl-col-5')">kl-col-5</a>
+              </li>
+              <li>
+                <a href="javascript:;" draggable="true" @dragstart="dragStart($event, 'kl-col-6')">kl-col-6</a>
+              </li>
+              <li>
+                <a href="javascript:;" draggable="true" @dragstart="dragStart($event, 'kl-col-7')">kl-col-7</a>
+              </li>
+              <li>
+                <a href="javascript:;" draggable="true" @dragstart="dragStart($event, 'kl-col-8')">kl-col-8</a>
+              </li>
+              <li>
+                <a href="javascript:;" draggable="true" @dragstart="dragStart($event, 'kl-col-9')">kl-col-9</a>
+              </li>
+              <li>
+                <a href="javascript:;" draggable="true" @dragstart="dragStart($event, 'kl-col-10')">kl-col-10</a>
+              </li>
+              <li>
+                <a href="javascript:;" draggable="true" @dragstart="dragStart($event, 'kl-col-11')">kl-col-11</a>
+              </li>
+              <li>
+                <a href="javascript:;" draggable="true" @dragstart="dragStart($event, 'kl-col-12')">kl-col-12</a>
+              </li>
             </ul>
         </div>
         <div class="g-main" @dragenter="drageEnter" @dragover.prevent="" @dragleave="dragLeave" @drop="drop" ref="preview">
 
         </div>
         <div class="g-rsb">
-            <ul>
-              <li>
-                <button @click="refresh('kl-card')">kl-card</button>
-              </li>
-              <li>
-                <button @click="refresh('kl-button')">kl-button</button>
-              </li>
-              <li>
-                <button @click="refresh('kl-input')">kl-input</button>
-              </li>
-            </ul>
+
         </div>
     </div>
 </template>
@@ -68,11 +97,16 @@ export default {
     },
     drop(event) {
       const path = event.path;
-      const componentsName = event.dataTransfer.getData('text');
+      let componentsName = event.dataTransfer.getData('text');
+      let slot = '';
+      if(componentsName.indexOf('kl-col') > -1) {
+        slot = componentsName.split('-').pop();
+        componentsName = 'kl-col';
+      }
       let flag = false;
       for(let i = 0; i < path.length; i++) {
         if(path[i].className && path[i].className.indexOf('r-tag') > -1) {
-          this.refresh(componentsName, path[i]);
+          this.refresh(componentsName, path[i], slot);
           flag = true;
           break;
         }
@@ -90,7 +124,7 @@ export default {
       });
       RootComponent.$inject(node);
     },
-    refresh(flag, node) {
+    refresh(flag, node, slot) {
       let tpl = '';
       switch(flag) {
         case 'kl-input':
@@ -102,8 +136,14 @@ export default {
         case 'kl-card':
           tpl = '<kl-card title="用户信息" ref="root-card" class="root-card r-tag" r-tag="root-card"></kl-card>';
           break;
+        case 'kl-row':
+          tpl = '<kl-row type="flex" gutter="0" class="root-row r-tag"></kl-row>';
+          break;
+        case 'kl-col':
+          tpl = `<kl-col span="${slot}" class="root-col r-tag"></kl-col>`;
+          break;
         default:
-          tpl = '<kl-card title="用户信息" ref="root-card" class="root-card r-tag" r-tag="root-card"></kl-card>';
+          tpl = '<kl-row type="flex" gutter="0" class="root-row r-tag"></kl-row>';
           break;
       }
       this.render(tpl, node);
@@ -112,7 +152,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
 .g-lsb {
     position: absolute;
     left: 0;
@@ -127,8 +167,9 @@ export default {
     display: inline-block;
     left: 120px;
     right: 120px;
-    width: calc(100vw - 240px);
-    height: calc(100vh - 60px);
+    width: calc(100vw - 260px);
+    height: calc(100vh - 80px);
+    margin: 10px;
     overflow: auto;
 }
 .g-rsb {
@@ -139,5 +180,13 @@ export default {
     height: calc(100vh - 60px);
     border-left: 1px solid #ccc;
     overflow: auto;
+}
+.g-row {
+  min-height: 50px;
+  border: 1px dashed #ddd;
+}
+.g-col {
+  min-height: 45px;
+  border: 1px dashed #ddd;
 }
 </style>
