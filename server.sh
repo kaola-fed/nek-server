8f908bf884bc1c18a2945891ff829da34f7e5fe0
lzxhahaha <lzxglhf@live.com>
2017-11-08 15:06:39 +0800

    update readme

--- server.sh
+++ server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export SESSION_KEY=koa_sess;
-export CLIENT_ID=cacdfca6c3b711e7bc6e5cf3fc793244;
-export CLIENT_SECRET=0b4a177ea4144324a240c14e3f4daaf7cace02dcc3b711e7bc6e5cf3fc793244;
+export CLIENT_ID=xxx;
+export CLIENT_SECRET=xxx;
 npm $*
