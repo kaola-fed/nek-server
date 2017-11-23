c61a3b9675e482862e3f48506587610452c1b5b9
soonwang <wangsong3635@outlook.com>
2017-11-23 21:10:54 +0800

    注释掉用户项目控制

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,6 +1,9 @@
 import UserModel from '../models/User';
+import ProjectModel from '../models/Project';
 
 export const dashboard = async (username) => {
-  const result = await UserModel.findByUsername(username);
-  return result.projects;
+  // const result = await UserModel.findByUsername(username);
+  // return result.projects;
+  const result = await ProjectModel.selectAll();
+  return result;
 };
